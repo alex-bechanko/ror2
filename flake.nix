e7cb2e39f7df60cee5f78caba041b8a0e9ff079c
Alex Bechanko <alexbechanko@gmail.com>
2022-11-15 16:11:41 -0500

    Add bash prompt prefix to nix flake

--- flake.nix
+++ flake.nix
@@ -6,7 +6,8 @@
   inputs.y2j.url = "github:alex-bechanko/y2j";
   inputs.gojsonschema-cli.url = "github:alex-bechanko/gojsonschema-cli";
   inputs.static-file-server.url = "github:alex-bechanko/static-file-server";
-  outputs = { self, nixpkgs, flake-utils, y2j, gojsonschema-cli, static-file-server }:
+  outputs =
+    { self, nixpkgs, flake-utils, y2j, gojsonschema-cli, static-file-server }:
     flake-utils.lib.eachDefaultSystem (system: {
       devShells.default = nixpkgs.legacyPackages.${system}.mkShell {
         buildInputs = [
@@ -20,5 +21,7 @@
       formatter = nixpkgs.legacyPackages.${system}.nixfmt;
 
     });
+
+  nixConfig.bash-prompt-prefix = "\\e[1m\\e[35mnix\\e[0m ";
 }
 
