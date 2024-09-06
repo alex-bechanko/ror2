47ad3e762f8fd54a209ec37e39777e48e09ce1d4
Alex Bechanko <alexbechanko@gmail.com>
2024-09-06 13:58:53 -0400

    Add ror2-frontend to flake.nix
    Nix can now handle building the frontend.
    This means that this site can be built in other places than just this
    development directory, a huge win!

--- flake.nix
+++ flake.nix
@@ -13,6 +13,7 @@
     { self, nixpkgs, flake-utils, y2j, gojsonschema-cli, static-file-server }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
+      flake-pkgs = self.packages.${system};
       in {
         packages.json-to-javascript = pkgs.writeShellApplication {
           name = "json-to-javascript";
@@ -34,11 +35,55 @@
           source = true;
         };
 
-        packages.ror2-frontend = ./web/default.nix;
+        packages.ror2-frontend = pkgs.stdenv.mkDerivation {
+          name = "ror2-frontend";
+          src = pkgs.lib.cleanSource ./web;
+          buildInputs = [
+            pkgs.dhall-json
+            pkgs.elmPackages.elm
+            pkgs.rsync
+            pkgs.nodePackages_latest.tailwindcss
+            flake-pkgs.json-to-javascript
+            flake-pkgs.ror2-web-data
+          ];
+
+          configurePhase = pkgs.elmPackages.fetchElmDeps {
+            elmPackages = import ./nix/elm-srcs.nix;
+            elmVersion = "0.19.1";
+            registryDat = ./nix/registry.dat;
+          };
+
+          installPhase =
+          ''
+          mkdir -p $out
+          mkdir -p $out/js
+
+          cp config.js $out/js/
+          elm make src/Main.elm --output $out/js/main.js
+
+          ${pkgs.dhall-json}/bin/dhall-to-json \
+            --file ${flake-pkgs.ror2-web-data}/source.dhall \
+            --output $out/js/catalog.js \
+            --pretty \
+            --preserve-null
+
+          ${flake-pkgs.json-to-javascript}/bin/json-to-javascript catalog $out/js/catalog.js
+
+          cp index.html $out/
+
+          mkdir -p $out/css
+          ${pkgs.nodePackages_latest.tailwindcss}/bin/tailwindcss \
+            -i input.css \
+            -o $out/css/style.css \
+            -c tailwind.config.js
+
+            rsync -ah images/ $out/images/
+          '';
+        };
 
         devShells.default = pkgs.mkShell {
           buildInputs = [
-            self.packages.${system}.json-to-javascript
+            flake-pkgs.json-to-javascript
             y2j.packages.${system}.y2j
             gojsonschema-cli.packages.${system}.gojsonschema-cli
             static-file-server.packages.${system}.static-file-server
